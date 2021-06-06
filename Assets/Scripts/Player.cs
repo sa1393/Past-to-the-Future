95afe0bb28a84b7b770c1627c0f03bc033579e04
gujerbit <ghftm1124@gmail.com>
2021-06-06 23:52:25 +0900

    플레이어 방향 전환, 플레이어 이동, 피격, 점프 애니메이션 추가 (2021-06-06) by gujerbit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,26 +8,32 @@ public class Player : MonoBehaviour
     public float moveSpeed = 45000f;
     public float jumpForce = 2000f;
 
-
     //체력
     public int hp;
     //공격력
     public int attackDamage;
 
     private Rigidbody2D rigid;
+    private Animator animator;
     public Collider2D playerCollider;
-
-
 
     //점프중
     private bool isJump = false;
     //점프 횟수
     private int jumpCount = 0;
+    //현재 플레이어가 오른쪽을 바라보고 있는가?
+    private bool isRight = true;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<PolygonCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        
     }
 
     private void Init() {
@@ -39,23 +45,24 @@ public class Player : MonoBehaviour
     {
         if (Input.GetButton("Jump") && !isJump)
         {
-            jump();
-
+            Jump();
         }
-
     }
 
     private void FixedUpdate()
     {
-        groundCheck();
-        playerMove();
+        GroundCheck();
+        PlayerMove();
     }
-
-  
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Enemy")
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
         {
             OnDamaged(collision.transform.position);
         }
@@ -67,7 +74,7 @@ public class Player : MonoBehaviour
     }
 
     //땅 체크
-    void groundCheck()
+    void GroundCheck()
     {
         Debug.DrawRay(rigid.position, Vector3.down * 110f, new Color(0, 1, 0));
 
@@ -80,35 +87,43 @@ public class Player : MonoBehaviour
     }
 
     //플레이어 이동
-    void playerMove()
+    void PlayerMove()
     {
         float axis_X = Input.GetAxis("Horizontal");
+
         if(axis_X != 0)
         {
             rigid.velocity = new Vector2(0, rigid.velocity.y);
             rigid.AddForce(Vector2.right * axis_X * moveSpeed);
-
         }
 
+        if(axis_X < 0 && isRight || axis_X > 0 && !isRight)
+        {
+            Flip();
+        }
 
-        // 속도 제한
+        //속도 제한
         if (rigid.velocity.x >= moveMaxSpeed)
         {
             rigid.velocity = new Vector2(2.5f, rigid.velocity.y);
-
         }
         else if (rigid.velocity.x <= moveMaxSpeed * -1)
         {
             rigid.velocity = new Vector2(-2.5f, rigid.velocity.y);
         }
+        //애니메이션 파라미터 설정
+        animator.SetFloat("axis_X", Mathf.Abs(axis_X));
     }
 
     //플레이어 점프
-    void jump()
+    void Jump()
     {
         isJump = true;
         rigid.velocity = Vector2.zero;
         rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        //애니메이션 파라미터 설정
+        animator.SetTrigger("isJump");
     }
 
     //플레이어 피격 이벤트
@@ -120,7 +135,17 @@ public class Player : MonoBehaviour
         //int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
         //Debug.Log(dirc);
         //rigid.AddForce(new Vector2(dirc, 1)*20, ForceMode2D.Impulse);
+
+        //애니메이션 파라미터 설정
+        animator.SetTrigger("isHit");
     }
 
-
+    //플레이어 스프라이트 전환
+    void Flip()
+    {
+        isRight = !isRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
 }
